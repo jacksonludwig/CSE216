41c83e4f4bc6b4c3c29d21b6e4ff857c67664988
Jackson Ludwig <jacksonludwig0@gmail.com>
2020-09-21 18:29:16 -0400

    package hw

---
+++ hw1/hwpackage/hw1math.ml
@@ -0,0 +1,20 @@
+(* Question 2.2 *)
+type expr =
+  | Const of int
+  | Var of string
+  | Plus of args
+  | Mult of args
+  | Minus of args
+  | Div of args
+and args = {arg1:expr; arg2:expr}
+
+
+(* Question 2.3 *)
+let rec evaluate arith_expr =
+  match arith_expr with
+  | Const value -> value
+  | Plus {arg1=a1; arg2=a2} -> (evaluate a1) + (evaluate a2)
+  | Mult {arg1=a1; arg2=a2} -> (evaluate a1) * (evaluate a2)
+  | Minus {arg1=a1; arg2=a2} -> (evaluate a1) - (evaluate a2)
+  | Div {arg1=a1; arg2=a2} -> (evaluate a1) / (evaluate a2)
+  | _ -> failwith "expression couldn't be evaluated"
