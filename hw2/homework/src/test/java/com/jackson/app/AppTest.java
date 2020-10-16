1b1f55f07a37421d156564a643577d129f497aa8
Jackson Ludwig <jacksonludwig0@gmail.com>
2020-10-16 18:36:42 -0400

    add tests for rect area and member

--- hw2/homework/src/test/java/com/jackson/app/AppTest.java
+++ hw2/homework/src/test/java/com/jackson/app/AppTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertTrue;
 import java.util.ArrayList;
 
 import com.jackson.model.Cuboid;
+import com.jackson.model.Rectangle;
 import com.jackson.model.ThreeDPoint;
 import com.jackson.model.TwoDPoint;
 
@@ -37,8 +38,40 @@ public class AppTest {
     }
 
     @Test
-    public void testEquals() {
-        TwoDPoint p1 = new TwoDPoint(1.2, 2.3);
-        assertTrue(p1 != p1);
+    public void testRectMember() {
+        ArrayList<TwoDPoint> points = new ArrayList<>();
+        points.add(new TwoDPoint(1, 1));
+        points.add(new TwoDPoint(0, 1));
+        points.add(new TwoDPoint(0, 0));
+        points.add(new TwoDPoint(1, 0));
+        Rectangle r = new Rectangle(points);
+
+        ArrayList<TwoDPoint> points2 = new ArrayList<>();
+        points2.add(new TwoDPoint(2, 2));
+        points2.add(new TwoDPoint(0, 2));
+        points2.add(new TwoDPoint(0, 0));
+        points2.add(new TwoDPoint(2, 0));
+        Rectangle r2 = new Rectangle(points2);
+
+        assertTrue(r.isMember(points2));
+    }
+
+    @Test
+    public void testRectArea() {
+        ArrayList<TwoDPoint> points = new ArrayList<>();
+        points.add(new TwoDPoint(1, 1));
+        points.add(new TwoDPoint(0, 1));
+        points.add(new TwoDPoint(0, 0));
+        points.add(new TwoDPoint(1, 0));
+        Rectangle r = new Rectangle(points);
+
+        ArrayList<TwoDPoint> points2 = new ArrayList<>();
+        points2.add(new TwoDPoint(2, 2));
+        points2.add(new TwoDPoint(0, 2));
+        points2.add(new TwoDPoint(0, 0));
+        points2.add(new TwoDPoint(2, 0));
+        Rectangle r2 = new Rectangle(points2);
+
+        assertTrue(r.area() != r2.area());
     }
 }
