5b3d2cfebb52540595daed395617b5add3750850
Jackson Ludwig <jacksonludwig0@gmail.com>
2020-10-17 12:32:01 -0400

    add sphere

---
+++ hw2/homework/src/main/java/com/jackson/model/Sphere.java
@@ -0,0 +1,44 @@
+package com.jackson.model;
+
+public class Sphere implements ThreeDShape {
+
+    private ThreeDPoint center;
+    private double radius;
+
+    public Sphere(double centerx, double centery, double centerz, double radius) {
+        this.center = new ThreeDPoint(centerx, centery, centerz);
+        this.radius = radius;
+    }
+
+    @Override
+    public int compareTo(ThreeDShape shape) {
+        return (int) (this.volume() - shape.volume());
+    }
+
+    @Override
+    public Point center() {
+        return this.center;
+    }
+
+    @Override
+    public double volume() {
+        return (4 / 3) * Math.PI * Math.pow(radius, 3);
+    }
+
+    public void setCenter(ThreeDPoint center) {
+        this.center = center;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public ThreeDPoint getCenter() {
+        return center;
+    }
+
+}
