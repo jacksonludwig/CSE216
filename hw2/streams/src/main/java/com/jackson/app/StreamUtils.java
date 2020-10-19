3af1856650d91b97dafec13bbf777fe79c25d1c9
Jackson Ludwig <jacksonludwig0@gmail.com>
2020-10-19 18:15:11 -0400

    make longest actually functional

--- hw2/streams/src/main/java/com/jackson/app/StreamUtils.java
+++ hw2/streams/src/main/java/com/jackson/app/StreamUtils.java
@@ -1,6 +1,5 @@
 package com.jackson.app;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
@@ -30,18 +29,32 @@ public class StreamUtils {
      *         where ties are broken based on <code>from_start</code>.
      */
     public static String longest(Collection<String> strings, boolean from_start) {
-        return strings.stream().reduce("", (currentMax, element) -> {
-                if (currentMax.length() == element.length()) {
-                   if (from_start)
-                       return currentMax;
-                   else
-                       return element;
-                }
+        return strings.stream().reduce(from_start ? (s1, s2) -> s1.length() >= s2.length() ? s1 : s2
+                : (s1, s2) -> s1.length() > s2.length() ? s1 : s2).orElse(null);
+    }
 
-                if(currentMax.length() > element.length())
-                    return currentMax;
-                else
-                    return element;
-        });
+    /**
+     * Find and return the least element from a collection of given elements that
+     * are comparable.
+     *
+     * @param items:      the given collection of elements
+     * @param from_start: a <code>boolean</code> flag that decides how ties are
+     *                    broken.
+     *
+     *                    If <code>true</code>, the element encountered earlier in
+     *                    the
+     *
+     *                    iteration is returned, otherwise the later element is
+     *                    returned.
+     * @param <T>:        the type parameter of the collection (i.e., the items are
+     *                    all of type T).
+     * @return the least element in <code>items</code>, where ties are
+     *
+     *         broken based on <code>from_start</code>.
+     */
+    public static <T extends Comparable<T>> T least(Collection<T> items, boolean from_start) {
+        // items.stream().reduce();
+
+        return null;
     }
 }
