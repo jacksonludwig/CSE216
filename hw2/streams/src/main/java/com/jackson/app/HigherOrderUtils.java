bdaff0abe3125f5dcc56bebad1967080ea6ba6bc
Jackson Ludwig <jacksonludwig0@gmail.com>
2020-10-19 23:40:48 -0400

    finish bifunc

--- hw2/streams/src/main/java/com/jackson/app/HigherOrderUtils.java
+++ hw2/streams/src/main/java/com/jackson/app/HigherOrderUtils.java
@@ -33,4 +33,31 @@ public class HigherOrderUtils {
             }
         };
 
+    public static NamedBiFunction<Double, Double, Double> multiply =
+        new NamedBiFunction<Double, Double, Double>() {
+            @Override
+            public Double apply(Double x, Double y) {
+                return x * y;
+            }
+
+            @Override
+            public String name() {
+                return "mult";
+            }
+        };
+
+    public static NamedBiFunction<Double, Double, Double> divide =
+        new NamedBiFunction<Double, Double, Double>() {
+            @Override
+            public Double apply(Double x, Double y) {
+                if (y == 0)
+                    throw new ArithmeticException("Cannot divide by zero");
+                return x / y;
+            }
+
+            @Override
+            public String name() {
+                return "div";
+            }
+        };
 }
