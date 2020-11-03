cadfd09fc225e9601ddb782f335f9cb17e5d9562
Jackson Ludwig <jacksonludwig0@gmail.com>
2020-11-03 16:24:28 -0500

    u

--- hw3/hw3-codebase/temp_main.py
+++ hw3/hw3-codebase/temp_main.py
@@ -1,9 +1,9 @@
 from two_d_point import TwoDPoint
-from typing import List
 
 points = TwoDPoint.from_coordinates([2, 2, 3.0, 3.1, 2.3, 1.2])
 for p in points:
     print(p)
+
 
 p1 = TwoDPoint(2, 2)
 p2 = TwoDPoint(4, 4)
