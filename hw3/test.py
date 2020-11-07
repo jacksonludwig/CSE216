36846287da53a5b2ee542f8d9fdb585cdde89723
Jackson Ludwig <jacksonludwig0@gmail.com>
2020-11-06 23:59:58 -0500

    test center and area for rectangle

--- hw3/test.py
+++ hw3/test.py
@@ -1,6 +1,8 @@
 from package.test import test_twoDPoint
 from package.test import test_quadrilateral
+from package.test import test_rectangle
 import unittest
 
 unittest.main(test_twoDPoint, exit=False)
-unittest.main(test_quadrilateral)
+unittest.main(test_quadrilateral, exit=False)
+unittest.main(test_rectangle)
