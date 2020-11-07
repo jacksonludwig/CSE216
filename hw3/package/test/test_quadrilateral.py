21db90a0fc432475b28780aa56fd11a400d9fa8a
Jackson Ludwig <jacksonludwig0@gmail.com>
2020-11-06 19:36:00 -0500

    finish quad tests

--- hw3/package/test/test_quadrilateral.py
+++ hw3/package/test/test_quadrilateral.py
@@ -1,16 +1,16 @@
+from package.project.two_d_point import TwoDPoint
 from unittest import TestCase
 from ..project.quadrilateral import Quadrilateral
 
 
 class TestQuadrilateral(TestCase):
-
     def test_side_lengths(self):
-        q = Quadrilateral(0, 1, 1, 1, 1, 0, 0, 0) # was TODO
+        q = Quadrilateral(0, 1, 1, 1, 1, 0, 0, 0)  # was TODO
         self.assertEqual((1, 1, 1, 1), q.side_lengths())
         self.assertNotEqual((-1, -1, -1, -1), q.side_lengths())
 
     def test_smallest_x(self):
-        q = Quadrilateral(0, 1, 1, 1, 1, 0, 0, 0) # was TODO
+        q = Quadrilateral(0, 1, 1, 1, 1, 0, 0, 0)  # was TODO
         self.assertEqual(0, q.smallest_x())
         q = Quadrilateral(0, 1, 1, 1, 1, -1, 0, 0)
         self.assertNotEqual(-1, q.smallest_x())
@@ -18,5 +18,23 @@ class TestQuadrilateral(TestCase):
         self.assertEqual(-2, q.smallest_x())
 
     def test___init__(self):
+        q = Quadrilateral(0, 1, 1, 1, 1, 0, 0, 0)
+        self.assertEqual(
+            q.vertices,
+            (TwoDPoint(0, 1), TwoDPoint(1, 1), TwoDPoint(1, 0), TwoDPoint(0, 0)),
+        )
         with self.assertRaises(ValueError):
             q = Quadrilateral(2.2, 3.1)
+
+    def test___eq__(self):
+        q = Quadrilateral(0, 1, 1, 1, 1, 0, 0, 0)
+        p = Quadrilateral(0, 1, 1, 1, 1, 0, 0, 1)
+        self.assertFalse(p == q)
+        q2 = Quadrilateral(0, 1, 1, 1, 1, 0, 0, 0)
+        p2 = Quadrilateral(0, 1, 1, 1, 1, 0, 0, 0)
+        self.assertTrue(q2 == p2)
+        self.assertTrue(p2 == p2)
+
+    def test___str__(self):
+        q = Quadrilateral(0, 1, 1, 1, 1, 0, 0, 0)
+        self.assertEqual("Quadrilateral: (0, 1); (1, 1); (1, 0); (0, 0)", str(q))
