735a5eda0a3accefc0a795c9421450c7327b464a
Jackson Ludwig <jacksonludwig0@gmail.com>
2020-11-07 18:59:10 -0500

    square test

--- hw3/package/test/test_square.py
+++ hw3/package/test/test_square.py
@@ -1,6 +1,15 @@
+from package.project.square import Square
 from unittest import TestCase
 
-def TestSquare(TestCase):
+class TestSquare(TestCase):
 
-    def test_snap():
-        pass
+    def test_snap(self):
+        s = Square(0, 1, 1, 1, 1, 0, 0, 0)
+        s2 = Square(0, .6, .6, .6, .6, 0, 0, 0)
+        s3 = Square(0, .4, .4, .4, .4, 0, 0, 0)
+        self.assertNotEqual(s, s2)
+        self.assertEqual(s, s2.snap())
+        self.assertNotEqual(s, s3.snap())
+        self.assertNotEqual(s3, s3.snap()) # although the points are the same, the shape is not, so it should not be equal
+        self.assertNotEqual(s, s3)
+        self.assertEqual(s, s.snap())
