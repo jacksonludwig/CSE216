5ed23d8e859c2547bc2109313637adb3689fef97
Jackson Ludwig <jacksonludwig0@gmail.com>
2020-11-08 14:53:48 -0500

    reformat

--- hw3/package/test/test_twoDPoint.py
+++ hw3/package/test/test_twoDPoint.py
@@ -3,12 +3,15 @@ from ..project.two_d_point import TwoDPoint
 
 
 class TestTwoDPoint(TestCase):
-
     def test_from_coordinates(self):
-        self.assertEqual([TwoDPoint(0, 0), TwoDPoint(
-            1.2, 3.2), TwoDPoint(-5, 3)], TwoDPoint.from_coordinates([0, 0, 1.2, 3.2, -5, 3]))
-        self.assertNotEqual([TwoDPoint(0, 0), TwoDPoint(
-            1.2, 3.2), TwoDPoint(-5, 3)], TwoDPoint.from_coordinates([0, 0, 1.2, 3.2, -4, 3]))
+        self.assertEqual(
+            [TwoDPoint(0, 0), TwoDPoint(1.2, 3.2), TwoDPoint(-5, 3)],
+            TwoDPoint.from_coordinates([0, 0, 1.2, 3.2, -5, 3]),
+        )
+        self.assertNotEqual(
+            [TwoDPoint(0, 0), TwoDPoint(1.2, 3.2), TwoDPoint(-5, 3)],
+            TwoDPoint.from_coordinates([0, 0, 1.2, 3.2, -4, 3]),
+        )
 
     def test___eq__(self):
         point_1 = TwoDPoint(2, 2)
