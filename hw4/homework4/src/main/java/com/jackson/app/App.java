d8031256a6aca276addd786e9f0ca6de39f87463
Jackson Ludwig <jacksonludwig0@gmail.com>
2020-11-19 23:48:53 -0500

    switch to junit 5

--- hw4/homework4/src/main/java/com/jackson/app/App.java
+++ hw4/homework4/src/main/java/com/jackson/app/App.java
@@ -5,7 +5,7 @@ public class App {
         int[] v = {-5, 2, 0, 4};
         DensePolynomial p = new DensePolynomial(v);
         System.out.println(p.degree());
-        System.out.println(p.getCoefficient(-1));
+        System.out.println(p.getCoefficient(1));
         int[] v2 = { 1 };
         DensePolynomial p2 = new DensePolynomial(v2);
         System.out.println(p2.degree());
---
+++ hw4/homework4/src/test/java/com/jackson/app/DensePolynomialTest.java
@@ -0,0 +1,12 @@
+package com.jackson.app;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import org.junit.jupiter.api.Test;
+
+public class DensePolynomialTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        // assertAll();
+    }
+}
