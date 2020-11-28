8c51025a0e2408d6e46743d533174ee1e336834f
Jackson Ludwig <jacksonludwig0@gmail.com>
2020-11-28 10:49:59 -0500

    u

--- hw4/homework4/src/test/java/com/jackson/app/DensePolynomialTest.java
+++ hw4/homework4/src/test/java/com/jackson/app/DensePolynomialTest.java
@@ -1,7 +1,10 @@
 package com.jackson.app;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -11,12 +14,14 @@ import org.junit.jupiter.api.TestInstance;
 public class DensePolynomialTest {
     Polynomial d1;
     Polynomial d2;
+    Polynomial d3;
     Polynomial s1;
 
     @BeforeAll
     public void setup() {
         d1 = new DensePolynomial("4x + 2");
         d2 = new DensePolynomial("2x^3 + 5x^2 + x + 20");
+        d3 = new DensePolynomial("0");
         s1 = new SparsePolynomial("-6x^2 + 3x + -1");
     }
 
@@ -39,5 +44,13 @@ public class DensePolynomialTest {
         assertEquals(4, d1.getCoefficient(1));
         assertEquals(2, d2.getCoefficient(3));
         assertEquals(20, d2.getCoefficient(0));
+        assertThrows(IllegalArgumentException.class, () -> d2.getCoefficient(-1));
+    }
+
+    @Test
+    public void testIsZero() {
+        assertTrue(d3.isZero());
+        assertFalse(d1.isZero());
+        assertFalse(d2.isZero());
     }
 }
