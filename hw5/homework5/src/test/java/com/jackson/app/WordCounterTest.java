04d3082fe70248ce8d70463981252c94ed67e5fc
Jackson Ludwig <jacksonludwig0@gmail.com>
2020-12-02 13:59:46 -0500

    hw5 init

---
+++ hw5/homework5/src/test/java/com/jackson/app/WordCounterTest.java
@@ -0,0 +1,12 @@
+package com.jackson.app;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class WordCounterTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
