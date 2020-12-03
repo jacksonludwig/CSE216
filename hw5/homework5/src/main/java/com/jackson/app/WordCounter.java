9279453915436c4ab6be7f568926bf1b1a7773fa
Jackson Ludwig <jacksonludwig0@gmail.com>
2020-12-03 16:37:45 -0500

    set up

--- hw5/homework5/src/main/java/com/jackson/app/WordCounter.java
+++ hw5/homework5/src/main/java/com/jackson/app/WordCounter.java
@@ -1,7 +1,26 @@
 package com.jackson.app;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class WordCounter {
+    // The following are the ONLY variables we will modify for grading.
+    // The rest of your code must run with no changes.
+
+    // path to the folder where input text files are located
+    public static final Path FOLDER_OF_TEXT_FILES = Paths.get("../../../../resources/words");
+    // path to the output plain-text (.txt) file
+    public static final Path WORD_COUNT_TABLE_FILE = Paths.get("../../../../resources/count/count.txt");
+    // max. number of threads to spawn
+    public static final int NUMBER_OF_THREADS = 2;
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+
+    public static void createExecutor() {
+        ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
     }
 }
