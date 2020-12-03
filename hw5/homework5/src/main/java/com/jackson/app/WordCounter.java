03066cdcd8e0b538625d4d82c378fd0e9335f8c9
Jackson Ludwig <jacksonludwig0@gmail.com>
2020-12-03 18:06:34 -0500

    u

--- hw5/homework5/src/main/java/com/jackson/app/WordCounter.java
+++ hw5/homework5/src/main/java/com/jackson/app/WordCounter.java
@@ -12,19 +12,17 @@ public class WordCounter {
     // The rest of your code must run with no changes.
 
     // path to the folder where input text files are located
-    public static final Path FOLDER_OF_TEXT_FILES =
-        Paths.get("../../../../resources/words");
+    public static final Path FOLDER_OF_TEXT_FILES = Paths.get("../../../../resources/words");
     // path to the output plain-text (.txt) file
     public static final Path WORD_COUNT_TABLE_FILE =
         Paths.get("../../../../resources/count/count.txt");
     // max. number of threads to spawn
     public static final int NUMBER_OF_THREADS = 2;
 
-    public static ExecutorService executorService =
-        Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+    public static ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
     public static Map<String, Integer> data = new ConcurrentHashMap<>();
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) { 
+        System.out.println("Hello World!"); 
     }
 }
