ebe8ce5f08a50b41bf5b25e8cdf7a74d6cf41fdb
Jackson Ludwig <jacksonludwig0@gmail.com>
2020-12-04 12:16:37 -0500

    u

--- hw5/homework5/src/main/java/com/jackson/app/WordCounter.java
+++ hw5/homework5/src/main/java/com/jackson/app/WordCounter.java
@@ -1,11 +1,18 @@
 package com.jackson.app;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class WordCounter {
     // The following are the ONLY variables we will modify for grading.
@@ -20,10 +27,21 @@ public class WordCounter {
     public static final int NUMBER_OF_THREADS = 2;
 
     public static ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
-    // filename, <word, amount>
+    // <filename, <word, amount>
     public static Map<String, ConcurrentHashMap<String, Integer>> data = new ConcurrentHashMap<>();
 
-    public static void main(String[] args) { 
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World!"); 
+        List<File> files = getListOfFiles();
+    }
+
+    public static List<File> getListOfFiles() throws IOException {
+        List<File> files;
+        try (Stream<Path> walk = Files.walk(FOLDER_OF_TEXT_FILES)) {
+            files = walk.filter(f -> Files.isRegularFile(f))
+                        .map(f -> f.toFile())
+                        .collect(Collectors.toList());
+        }
+        return files;
     }
 }
